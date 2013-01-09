ce5520f21179dc319ffc1d3a35a9af594d19a6ea
stbalint <balint.steinbach@inepex.com>
2013-01-09 14:56:23 +0100

    setting panel position style

--- src/gwtl-widget/src/main/java/org/discotools/gwt/leaflet/client/widget/PaneAbsolutePanel.java
+++ src/gwtl-widget/src/main/java/org/discotools/gwt/leaflet/client/widget/PaneAbsolutePanel.java
@@ -1,6 +1,7 @@
 package org.discotools.gwt.leaflet.client.widget;
 
 
+import com.google.gwt.dom.client.Style.Position;
 import com.google.gwt.user.client.Element;
 import com.google.gwt.user.client.ui.AbsolutePanel;
 
@@ -8,6 +9,7 @@ public class PaneAbsolutePanel extends AbsolutePanel {
 
 	public PaneAbsolutePanel(Element element) {
 		super(element);
+		getElement().getStyle().setPosition(Position.RELATIVE);
 		onAttach();
 	}
 	
